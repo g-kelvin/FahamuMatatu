62f1b504d6ab2b73097a080f80ccbb6342a03019
g-kelvin <kelvingauki@gmail.com>
2018-09-20 23:39:56 +0300

    This is a commit to deploy FahamuMatatu to Heroku
    Deploy fahamu matatu to heroku for the first time.

--- matatu/settings.py
+++ matatu/settings.py
@@ -126,3 +126,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 # STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
